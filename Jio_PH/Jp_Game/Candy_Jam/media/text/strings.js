ea7eda1bac264089808bcc1f072ec26c84960ba8
Arka2jio <95009397+Arka2jio@users.noreply.github.com>
2022-01-13 20:28:37 +0530

    Add files via upload

---
+++ Jio_PH/Jp_Game/Candy_Jam/media/text/strings.js
@@ -0,0 +1,82 @@
+var _STRINGS = {
+	"Ad":{
+		"Mobile":{
+			"Preroll":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"Header":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"End":{
+				"ReadyIn":"Advertisement ends in ",
+				"Loading":"Please wait ...",
+				"Close":"Close",
+			},								
+		},
+	},
+	
+	"Splash":{
+		"Loading":"Loading ...",	
+		"LogoLine1":"Some text here",
+		"LogoLine2":"powered by MarketJS",
+		"LogoLine3":"none",						
+	},
+
+	"Game":{
+		"SelectPlayer":"Select Player",
+		"Win":"You win!",
+		"Lose":"You lose!",
+		"Score":"Score",
+		"Time":"Time",
+		"GameOverCapt":"GAME OVER",
+		"Completed":"You have completed",
+		"Level":"Level",
+		"LevelCapt":"LEVEL",
+		"NoMoves":"No more moves",
+		"HighScores":"High Scores",
+		"ScoreCapt":"SCORE",
+		"CollectCandies":"Collect These Candies",
+		"MusicCapt":"MUSIC",
+		"SoundCapt":"SOUND",
+		"Paused":"PAUSED",
+		"KeyHint":"Press Center key",
+		"Control1a":"Use directional keys",
+		"Control1b":"to move cursor around",
+		"Control2":"Then press Center key to select candy",
+		"Control3a":"When candy is selected",
+		"Control3b":"press Left/Right/Up or Down key",
+		"Control3c":"to swap in that direction",
+		"Control4a":"Well done !",
+		"Control4b":"Now try to match 3 or more of",
+		"Control4c":"the same candy to score points"
+	},
+
+	"Results":{
+		"Title":"High score",
+	},	
+	
+	"Menu":
+	{
+		"Control1": "Use Up or Down key to navigate menu",
+		"Control2": "Press Center key to continue"
+	},
+	
+	"Sound":
+	{
+		"staticSound":"staticSound",
+		"openingSound":"openingSound",
+		"kittyopeningSound":"kittyopeningSound",
+		"click":"click",
+		"spark":"spark",
+		"swing":"swing",
+		"bomb":"bomb",
+		"board":"board",
+		"clocktick":"clocktick",
+		"timeout":"timeout",
+		"levelCompleted":"levelCompleted"
+	}
+};
